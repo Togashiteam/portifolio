b5b62a3ef01d9da0eda19917473c17ff49621123
Gustavo Calastro <gu.calastro@gmail.com>
2024-07-17 19:03:20 -0300

    Navegabilidade em fun,style e dev

--- src/app/fun/page.tsx
+++ src/app/fun/page.tsx
@@ -1,8 +1,31 @@
+import Link from "next/link";
 
 export default function Fun() {
   return (
     <>
-      <h1>Fun works</h1>
+      <Link href={"../"}>Home</Link>
+      <h1>Fun</h1>
+      <br />
+      <ul>
+        <li>
+          <Link href="fun/imc">IMC</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="fun/rockPaperScissors">Rock Paper Scissors</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="fun/workOutTimer">WorkOut Timer</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="fun/calc">Calculator</Link>
+        </li>
+      </ul>
     </>
   );
 }
