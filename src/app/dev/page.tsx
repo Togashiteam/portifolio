f1a41838662d4a96b57e23c57e1e5f345745e57c
Evandr0 <evandro_lr_@hotmail.com>
2024-07-18 10:45:42 -0300

    :sparkles: Adiciona referência das páginas (#17)
    On branch featute/ref-pages
    Changes to be committed:
    	modified:   src/app/dev/page.tsx
    	modified:   src/app/fun/page.tsx
    	modified:   src/app/fun/rockPaperScissors/page.tsx
    	modified:   src/app/fun/workOutTimer/page.tsx
    	modified:   src/app/style/page.tsx
    	modified:   yarn.lock

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Dev() {
   return (
     <>
-      <h1>DEV Works</h1>
+      <div className="flex justify-center mt-4 text-light-300  font-bold text-lg">
+        <Link href={"../"}>Home</Link>
+      </div>
+      <div className="flex justify-center items-center my-10 w-full h-10 bg-primary-300">
+        <div className="text-light-300 font-bold text-lg">
+          <h1>DEV Works</h1>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex justify-center items-center mx-2 w-48 h-40 bg-primary-300">
+          <div className="text-light-300 font-bold text-lg">
+            <Link href="dev/fonts">Fonts</Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
